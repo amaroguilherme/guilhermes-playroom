1b1aa9aa781b0ce041b9afeee78b3b0bb727ce82
Guilherme Alves <guilherme.amaro@ibm.com>
2021-03-31 10:18:06 -0300

    refac on landing page structure and sign up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page/landing-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule } from '@angular/material';
+import { SignUpComponent } from './sign-up/sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  isSignUp = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  signUp() {
+    this.isSignUp = true;
+  }
+
+  logIn() {
+    this.isSignUp = false;
   }
 
 }
