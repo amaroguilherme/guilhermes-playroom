d9d2628b0045acbd8ecc9417079e1318bc087fb9
Guilherme Alves <guilherme.amaro@ibm.com>
2021-04-02 16:21:40 -0300

    creates loading service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatProgressSpinnerModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { SignUpComponent } from './sign-up/sign-up/sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
@@ -17,6 +17,8 @@ import { FeedComponent } from './feed/feed.component';
 import { GamesComponent } from './games/games.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MusicComponent } from './music/music.component';
+import { LoadingComponent } from './shared/loading/loading.component';
+import { LoadingInterceptorService } from './services/loading-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,8 @@ import { MusicComponent } from './music/music.component';
     FeedComponent,
     GamesComponent,
     MoviesComponent,
-    MusicComponent
+    MusicComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
@@ -45,9 +48,16 @@ import { MusicComponent } from './music/music.component';
     HttpClientModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatProgressSpinnerModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
