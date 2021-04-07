a854d514e697836c805b5a6cbf69c33091c2cd21
Guilherme Alves <guilherme.amaro@ibm.com>
2021-04-07 15:53:36 -0300

    creates loading animation

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -9,14 +9,13 @@ import { LoadingService } from 'src/app/services/loading.service';
 })
 export class LoadingComponent implements AfterViewInit, OnDestroy {
   loadingSubscription: Subscription;
-  currentMessage: string;
+  currentMessage: string = '...';
   messages = [
     'This might take a while, stay with me...',
     'I swear this is not ALL my fault.',
     'Promise you its not broken. We are almost there.',
     'Do you wanna hear a joke?',
-    'You know when developers say that it gonna be quick? Funny, right.',
-    'Listen. A lot its being done on the background. Do not be rude.',
+    'A lot its being done on the background. Do not be rude.',
     'Have you watched Pulp Fiction? Its pretty cool.',
     'Bananas suck. Banana yogurt its nice, though.'
   ]
